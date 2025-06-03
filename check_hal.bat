2ccb05c4d630d8868317aff347ed6a61898a3fff
Henri Bretel <henri.bretel@universite-paris-saclay.fr>
2025-06-03 16:04:10 +0200

    delete unnecessary

--- check_hal.bat
+++
@@ -1,2 +0,0 @@
-pip install -r requirements.txt
-python check_hal_collection.py
--- config.py
+++
@@ -1,9 +0,0 @@
-endpoint="http://api.archives-ouvertes.fr/search/"
-
-escapeRules ={'+':r'\+','-':r'\-','&':r'\&','|':r'\|','!':r'\!','(':r'\(',')':r'\)','{':r'\{','}':r'\}','[':r'\[',
-              ']':r'\]','^':r'\^','~':r'\~','*':r'\*','?':r'\?',':':r'\:','"':r'\"'}
-
-hal_fl="docid,doiId_s,title_s,submitType_s"
-
-default_start=2018
-default_end='*'
