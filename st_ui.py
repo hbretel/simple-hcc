6b26492bcf3446ef56f5e3bc31b2aefe50f389da
Henri Bretel <henri.bretel@universite-paris-saclay.fr>
2025-06-10 18:02:07 +0200

    fix path error

--- st_ui.py
+++ st_ui.py
@@ -3,9 +3,9 @@ from os import path
 
 def main():
     if "navigation" not in st.session_state:
-        st.navigation([st.Page(path.relpath(r"pages\file_upload.py"),title='HAL collection Checker')]).run()
+        st.navigation([st.Page(path.relpath("pages/file_upload.py"),title='HAL collection Checker')]).run()
     else: 
-        st.navigation([st.Page(path.relpath("pages\\"+st.session_state.navigation))]).run()
+        st.navigation([st.Page(path.relpath("pages/"+st.session_state.navigation))]).run()
 
 
 if __name__ == "__main__":
