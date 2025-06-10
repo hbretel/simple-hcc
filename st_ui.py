16a26e50d8bc6cdb6cd7763047f7b7d5924a6bf9
Henri Bretel <henri.bretel@universite-paris-saclay.fr>
2025-06-10 17:58:23 +0200

    try to fix navigation

--- st_ui.py
+++ st_ui.py
@@ -1,11 +1,12 @@
 import streamlit as st
 from os import path
 
-
 def main():
-    st.navigation([st.Page(path.relpath(r"pages/file_upload.py"),title='HAL collection Checker') 
-     if "navigation" not in st.session_state  
-     else st.Page(path.join(r"pages/",st.session_state.navigation))]).run()
+    if "navigation" not in st.session_state:
+        st.navigation([st.Page(path.relpath(r"pages\file_upload.py"),title='HAL collection Checker')]).run()
+    else: 
+        st.navigation([st.Page(path.relpath("pages\\"+st.session_state.navigation))]).run()
+
 
 if __name__ == "__main__":
     main()
